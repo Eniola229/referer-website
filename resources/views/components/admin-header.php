cc544be933013d56407f8401bbe7f025d925daca
Adeyemi Joshua <joshuaadeyemi445@gmail.com>
2024-09-18 13:09:26 +0100

    fixed side nav

--- resources/views/components/admin-header.php
+++ resources/views/components/admin-header.php
@@ -1,8 +1,7 @@
   <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/soft-ui-dashboard/pages/dashboard.html " target="_blank">
-        <img src="https://pbs.twimg.com/profile_images/1763219067400249344/p7yVhJ4j_400x400.jpg" class="navbar-brand-img h-100" alt="main_logo">
+      <a class="navbar-brand m-0" href="{{ route('/') }}" class="navbar-brand-img h-100" alt="main_logo">
         <span class="ms-1 font-weight-bold">AfricTv Admin Dashboard</span>
       </a>
     </div>
